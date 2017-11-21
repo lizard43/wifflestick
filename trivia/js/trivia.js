e0c1f9f528eac81cd4dea5d86616c7832d1cb5c8
d <lizard43@gmail.com>
2017-11-21 18:01:27 -0500

    update

--- trivia/js/trivia.js
+++ trivia/js/trivia.js
@@ -6,7 +6,7 @@ var request = new XMLHttpRequest();
 
 $(document).ready(function() {
 
-    var rnd = randomIntFromInterval(1, 1100);
+    var rnd = randomIntFromInterval(1, 12000);
 
     request.onload = function () {
 
